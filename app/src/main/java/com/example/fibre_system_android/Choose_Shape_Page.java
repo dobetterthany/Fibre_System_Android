523cfb2f1170c397673fba281fcff273b550cd2d
Joseph Kim <kimg009263@gmail.com>
2022-11-29 18:48:22 +1300

    choose size page created

--- app/src/main/java/com/example/fibre_system_android/Choose_Shape_Page.java
+++ app/src/main/java/com/example/fibre_system_android/Choose_Shape_Page.java
@@ -2,7 +2,9 @@ package com.example.fibre_system_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Choose_Shape_Page extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Choose_Shape_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_shape_page);
+    }
+
+    public void goChooseSizePage(View v) {
+        //launch a new activity
+        Intent i = new Intent(this, Choose_Size_Page.class);
+        startActivity(i);
     }
 }
