97be1739c71a0c6cbf9030abae9ae438fa1b9f52
lilzhuo <90330271+lilzhuo@users.noreply.github.com>
2023-02-06 01:02:22 +0800

    Update04/02/2023

--- app/src/main/java/com/example/fibre_system_android/Choose_Size_Page.java
+++ app/src/main/java/com/example/fibre_system_android/Choose_Size_Page.java
@@ -1,70 +1,69 @@
-package com.example.fibre_system_android;
+package com.example.testapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Choose_Size_Page extends AppCompatActivity {
 
-    private int dpHeight;
-    private int dpWidth;
-    private float dDensity;
+    private int Height;
+    private int Width;
+
     private int designWidth = 1280;
     private int designHeight = 800;
+
+    EditText widthInput;
+    EditText heightInput;
+    Button confirmButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_size_page);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.choose_size);
 
-        makeResponsive();
+        widthInput = (EditText) findViewById(R.id.widthInput);
+        heightInput = (EditText) findViewById(R.id.heightInput);
+
+        confirmButton = (Button) findViewById(R.id.confirmButton);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Height = Integer.valueOf(heightInput.getText().toString());
+                Width = Integer.valueOf(widthInput.getText().toString());
+            }
+
+
+        });
     }
+
+    public void AddImage(int imageID)
+    {
+
+        RelativeLayout.LayoutParams lParams = new RelativeLayout.LayoutParams(
+                ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+        lParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+        ImageView icon = new ImageView(context);
+        icon.setImageResource(imageID);
+        icon.setLayoutParams(lParams);
+    }
+
     public void goPlannerPage(View v) {
         //launch a new activity
-        Intent i = new Intent(this, Planner_Area_Page.class);
+        Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
 
-    private void makeResponsive() {
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
 
-        dpHeight = (displayMetrics.heightPixels);
-        dpWidth = (displayMetrics.widthPixels);
-        dDensity = (displayMetrics.scaledDensity);
-
-        Button button1 = (Button) findViewById(R.id.button5);
-        Button button2 = (Button) findViewById(R.id.button6);
-        Button button3 = (Button) findViewById(R.id.button7);
-        Button button4 = (Button) findViewById(R.id.button8);
-
-        ViewGroup.LayoutParams button1Params = (ViewGroup.MarginLayoutParams) button1.getLayoutParams();
-        button1Params.width = calcWidth(300);
-        button1Params.height = calcHeight(150);
-
-        ViewGroup.LayoutParams button2Params = (ViewGroup.MarginLayoutParams) button2.getLayoutParams();
-        button2Params.width = calcWidth(300);
-        button2Params.height = calcHeight(150);
-
-        ViewGroup.LayoutParams button3Params = (ViewGroup.MarginLayoutParams) button3.getLayoutParams();
-        button3Params.width = calcWidth(300);
-        button3Params.height = calcHeight(150);
-
-        ViewGroup.LayoutParams button4Params = (ViewGroup.MarginLayoutParams) button4.getLayoutParams();
-        button4Params.width = calcWidth(300);
-        button4Params.height = calcHeight(150);
-    }
-
-    public int calcHeight (float value) {
-        return (int) (dpHeight * (value/designHeight));
-    }
-
-    public int calcWidth(float value) {
-        return (int) (dpWidth * (value/designWidth));
-    }
 }
