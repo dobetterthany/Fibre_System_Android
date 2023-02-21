87ec338de5efcda0d1c426908ce73160943e6026
JosephKim <42214982+dobetterthany@users.noreply.github.com>
2023-02-22 00:50:45 +1300

    Update Planner_Area_Page.java
    Spinner deleted

--- app/src/main/java/com/example/fibre_system_android/Planner_Area_Page.java
+++ app/src/main/java/com/example/fibre_system_android/Planner_Area_Page.java
@@ -51,8 +51,6 @@ public class Planner_Area_Page extends AppCompatActivity implements AdapterView.
         bathroomPlannerLayout = new BathroomPlannerLayout(this, plannerArea);
         itemsArrayList  = new ArrayList<RecyclerViewItems>();
 
-        setSpinner();
-
         initSearchWidget();
 
 //        setRecyclerView();
@@ -68,14 +66,9 @@ public class Planner_Area_Page extends AppCompatActivity implements AdapterView.
         dpWidth = (displayMetrics.widthPixels);
         dDensity = (displayMetrics.scaledDensity);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
         SearchView searchView = (SearchView) findViewById(R.id.productListSearchView);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         Button finishButton = (Button) findViewById(R.id.button3);
-
-        ViewGroup.LayoutParams spinnerParams = (ViewGroup.MarginLayoutParams) spinner.getLayoutParams();
-        spinnerParams.width = calcWidth(300);
-        spinnerParams.height = calcHeight(60);
 
         ViewGroup.LayoutParams searchViewParams = (ViewGroup.MarginLayoutParams) searchView.getLayoutParams();
         searchViewParams.width = calcWidth(300);
@@ -142,17 +135,7 @@ public class Planner_Area_Page extends AppCompatActivity implements AdapterView.
 
         return super.onCreateOptionsMenu(menu);
     }
-    private void setSpinner() {
-        spinner = findViewById(R.id.spinner);
-        //gets items from products array in string file, and plug it into spinner item list
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.products, android.R.layout.simple_spinner_item);
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(this);
-    }
+    
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
