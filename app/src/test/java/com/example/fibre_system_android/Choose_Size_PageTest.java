f0176edb69a775e852af4b68789103e60a008a5a
Gyeongjae Kim <xtj6735@autuni.ac.nz>
2023-03-28 11:16:11 +1300

    Create Choose_Size_PageTest.java

---
+++ app/src/test/java/com/example/fibre_system_android/Choose_Size_PageTest.java
@@ -0,0 +1,18 @@
+package com.example.fibre_system_android;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Choose_Size_PageTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void addImage() {
+        Choose_Size_Page choose_size_page = new Choose_Size_Page();
+
+    }
+}
