b73d96d7598646761b2ce0a51774d644dbe53312
Gyeongjae Kim <xtj6735@autuni.ac.nz>
2023-03-28 13:04:58 +1300

    Unit testing(choose size page)

--- app/src/test/java/com/example/fibre_system_android/Choose_Size_PageTest.java
+++ app/src/test/java/com/example/fibre_system_android/Choose_Size_PageTest.java
@@ -1,18 +1,42 @@
 package com.example.fibre_system_android;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
 import org.junit.Test;
+
 
 public class Choose_Size_PageTest {
 
+    @Rule
+    public ActivityScenarioRule<Choose_Size_Page> rule =
+            new ActivityScenarioRule<>(Choose_Size_Page.class);
+
     @Test
     public void onCreate() {
+        Choose_Size_Page choose_size_page = new Choose_Size_Page();
+        ActivityScenario<Choose_Size_Page> scenario = rule.getScenario();
+        int maximumHeight = 2000;
+        int maximumWidth = 2000;
+
+        String heightString = choose_size_page.heightInput.getText().toString();
+        String widthString = choose_size_page.widthInput.getText().toString();
+        int height = Integer.parseInt(heightString);
+        int width = Integer.parseInt(widthString);
+        height = 200;
+        width = 198;
+
+        assertTrue(height<=maximumHeight);
+        assertTrue(maximumWidth>=width);
+        assertTrue(maximumHeight<height);
+        assertTrue(maximumWidth<width);
     }
 
     @Test
     public void addImage() {
-        Choose_Size_Page choose_size_page = new Choose_Size_Page();
 
     }
 }
