2e461d225a17bb083fc711a4fde87f01b3b76036
Gyeongjae Kim <xtj6735@autuni.ac.nz>
2023-03-28 14:46:27 +1300

    Unit testing (update)

--- app/src/test/java/com/example/fibre_system_android/Choose_Size_PageTest.java
+++ app/src/test/java/com/example/fibre_system_android/Choose_Size_PageTest.java
@@ -1,38 +1,45 @@
 package com.example.fibre_system_android;
 
-import static org.junit.Assert.assertTrue;
-
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-
-import org.junit.Rule;
+import org.junit.Assert;
 import org.junit.Test;
-
 
 public class Choose_Size_PageTest {
 
-    @Rule
-    public ActivityScenarioRule<Choose_Size_Page> rule =
-            new ActivityScenarioRule<>(Choose_Size_Page.class);
+    public Choose_Size_Page choose_size_page;
+
+
+
+//    @Rule
+//    public ActivityScenarioRule<Choose_Size_Page> rule =
+//            new ActivityScenarioRule<>(Choose_Size_Page.class);
+//
 
     @Test
     public void onCreate() {
-        Choose_Size_Page choose_size_page = new Choose_Size_Page();
-        ActivityScenario<Choose_Size_Page> scenario = rule.getScenario();
+//        ActivityScenario<Choose_Size_Page> scenario = rule.getScenario();
+        choose_size_page = new Choose_Size_Page();
         int maximumHeight = 2000;
         int maximumWidth = 2000;
+        int minHeight = 1000;
+        int minWidth = 1000;
 
-        String heightString = choose_size_page.heightInput.getText().toString();
-        String widthString = choose_size_page.widthInput.getText().toString();
-        int height = Integer.parseInt(heightString);
-        int width = Integer.parseInt(widthString);
-        height = 200;
-        width = 198;
+//
+//        String heightString = choose_size_page.heightInput.getText().toString();
+//        String widthString = choose_size_page.widthInput.getText().toString();
+//        int height = Integer.parseInt(heightString);
+//        int width = Integer.parseInt(widthString);
+//        height = 200;
+//        width = 198;
+//
+//
+        Assert.assertTrue(choose_size_page.Height <= maximumHeight);
+        Assert.assertTrue(choose_size_page.Width <= maximumWidth);
 
-        assertTrue(height<=maximumHeight);
-        assertTrue(maximumWidth>=width);
-        assertTrue(maximumHeight<height);
-        assertTrue(maximumWidth<width);
+        Assert.assertTrue(choose_size_page.Height >= minHeight);
+        Assert.assertTrue(choose_size_page.Width >= minWidth);
+
+
+
     }
 
     @Test
